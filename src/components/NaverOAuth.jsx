6ad1eb305a482a3f74038cc7abab12f253e7ab76
Jiwon <won3713e@naver.com>
2024-12-29 17:53:36 +0900

    1229naver

--- src/components/NaverOAuth.jsx
+++ src/components/NaverOAuth.jsx
@@ -1,12 +1,17 @@
 const NAVER_CLIENT_ID = "VTFXvAwu4tZjVecOcW1h";
-const NAVER_REDIRECT_URI = "http://localhost:3000/naver";
+const NAVER_REDIRECT_URI = encodeURIComponent("http://localhost:3000/naver");
 
 // state 생성
 const generateState = () => {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 };
 
-const STATE = generateState();
-// localStorage.setItem('naver_state', STATE);
+// localStorage에서 state 확인
+let STATE = localStorage.getItem('naver_state');
 
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${NAVER_REDIRECT_URI}`;
+if (!STATE) {
+  STATE = generateState();
+  localStorage.setItem('naver_state', STATE);
+}
+
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${STATE}`;
